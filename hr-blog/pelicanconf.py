a0cdd7cac3a68b1bc4d1a6405d065bbddf4f030d
hackrole <daipeng123456@gmail.com>
2015-05-25 13:37:09 +0800

    fix

--- hr-blog/pelicanconf.py
+++ hr-blog/pelicanconf.py
@@ -12,8 +12,8 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'cn'
 
-THEME = "/home/daipeng/Desktop/tmp/pelican-tmp/pelican-themes/alchemy"
-# THEME = "/home/daipeng/Desktop/tmp/pelican-tmp/pelican-themes/bootstrap2"
+# THEME = "/home/daipeng/Desktop/tmp/pelican-tmp/pelican-themes/alchemy"
+THEME = "/home/daipeng/Desktop/tmp/pelican-tmp/pelican-themes/bootstrap2"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,10 +24,6 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = []
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-         #('Python.org', 'http://python.org/'),
-         #('Jinja2', 'http://jinja.pocoo.org/'),
-         #('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -36,4 +32,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
