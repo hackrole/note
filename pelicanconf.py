12c32fac11676a40158859bb1c9795c7eb874e0a
hackrole <daipeng123456@gmail.com>
2018-11-21 11:48:02 +0800

    add gum and foundation-default-colours themes, use gum themes now

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,7 @@ BASE_DIR = path.dirname(path.realpath(__file__))
 AUTHOR = u'hackrole'
 SITENAME = u"hackrole's note"
 SITEURL = ''
+SITESUBTITLE = u"愿所有英雄气概都得以归宿"
 
 PATH = 'content'
 
@@ -45,11 +46,15 @@ EXTRA_PATH_METADATA = {
     },
 }
 
-# theme
-THEME = path.join(BASE_DIR, 'themes/new-bootstrap2')
-#THEME = 'new-bootstrap2'
-# THEME = 'pelican-hyde'
-#THEME = 'pelican-sober'
+# === theme ====
+# THEME = 'new-bootstrap2'
+THEME = path.join(BASE_DIR, 'themes/gum')
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# foundation-default-colours theme config
+# THEME = 'foundation-default-colours'
+# FOUNDATION_PYGMENT_THEME = 'vs'
 
 
 # Blogroll
@@ -62,7 +67,11 @@ LINKS = [
 SOCIAL = [
     ('github', 'http://github.com/hackrole'),
     ('coding', 'https://coding.net/u/hackrole'),
+    ('TWITTER_URL', ''),
+    ('FACEBOOK_URL', ''),
 ]
+GITHUB_URL = 'http://github.com/hackrole'
+
 
 PLUGIN_PATHS = [
     path.join(BASE_DIR, "plugins"),
@@ -71,6 +80,7 @@ PLUGINS = [
     # need: pip install disqus-python
     # "disqus_static",
     "series",
+    "tag_cloud",
     # "sitemap",
     "plantuml",
     # 'ga_page_view',
@@ -82,6 +92,11 @@ DISQUS_PUBLIC_KEY = ('X83kWZqXZ1xbOxi0QS2P9qnbxerhA5C59a80O7M'
                      'W3G0qsDOpEwklaAcR3oVcG0uv')
 DISQUS_SECRET_KEY = ('za2LQQnPg7oRPOYmVrFkEt2HD6KWIrjolR3icno'
                      'yxmqxSOaATH6MoFROXlrrFQqA')
+
+# tag cloud
+TAG_CLOUD_STEPS = 4
+TAG_CLOUD_MAX_ITEMS = 50
+TAG_CLOUD_BADGE = True
 
 # define sitemap
 # SITEMAP = {
